c1f17a874a17369c9bed59e46e8084d91c6f8405
José A.P <jose@clovrlabs.com>
2023-01-20 15:16:10 +0100

    Gauges working for each channel

--- go.mod
+++ go.mod
@@ -3,15 +3,21 @@ module liquidator
 go 1.19
 
 require (
-	google.golang.org/grpc v1.52.0
 	github.com/lightningnetwork/lnd/lnrpc v0.0.2
-)
-replace (
-	github.com/lightningnetwork/lnd/lnrpc => ./github.com/lightningnetwork/lnd/lnrpc
+	google.golang.org/grpc v1.52.0
 )
 
+replace github.com/lightningnetwork/lnd/lnrpc => ./github.com/lightningnetwork/lnd/lnrpc
+
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/prometheus/client_golang v1.14.0 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.37.0 // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
 	golang.org/x/net v0.5.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
--- main.go
+++ main.go
@@ -2,31 +2,91 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"net/http"
 	"time"
+
+	"github.com/lightningnetwork/lnd/lnrpc"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/metadata"
-	"github.com/lightningnetwork/lnd/lnrpc"
 )
+
+type metrics struct {
+	channelBalanceGauge prometheus.GaugeVec
+}
+
+func NewMetrics(reg prometheus.Registerer) *metrics {
+	m := &metrics{
+		channelBalanceGauge: *prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Name: "liquidator_channel_balance",
+			Help: "The total number of processed events",
+		},
+			[]string{"channel_id", "local_node_pubkey", "remote_node_pubkey", "local_node_alias", "remote_node_alias"},
+		),
+	}
+	reg.MustRegister(m.channelBalanceGauge)
+	return m
+}
 
 func main() {
 
+	//Create a new prometheus registry
+	reg := prometheus.NewRegistry()
+
+	metrics := NewMetrics(reg)
+
+	//Start a http server to expose metrics
+	http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{Registry: reg}))
+	go http.ListenAndServe(":9000", nil)
+
 	//Call connectToServer function with url of the server every minute
 	for {
-		connectToServer("localhost:10001")
+		connectToServer("localhost:10001", metrics)
 		time.Sleep(1 * time.Second)
 	}
 }
 
+// Record the channel balance in a prometheus gauge
+func recordChannelBalance(metrics *metrics, channel *lnrpc.Channel) {
+
+
+	channelBalanceRatioInt := float64(channel.GetLocalBalance()) / float64(channel.GetCapacity())
+
+	//Truncate channelbalance to 2 decimal places
+	channelBalanceRatio := float64(int(channelBalanceRatioInt*100)) / 100
+
+	//Check that the ration is between 0 and 1
+	if channelBalanceRatio > 1 || channelBalanceRatio < 0 {
+		log.Fatalf("Channel balance ratio is not between 0 and 1")
+	}
+
+	//ChannelId uint to string
+	channelId := fmt.Sprint(channel.GetChanId())
+
+	//Set the channel balance in the gauge
+	metrics.channelBalanceGauge.With(prometheus.Labels{
+		"channel_id": channelId,
+		"local_node_pubkey":  "123", //TODO
+		"remote_node_pubkey": channel.GetRemotePubkey(),
+		"local_node_alias":    "alice",
+		"remote_node_alias":   "bob"}).Set(channelBalanceRatio)
+
+	time.Sleep(2 * time.Second)
+
+}
+
 // func that receives a url and connects to a grpc server
-func connectToServer(url string) {
+func connectToServer(url string, metrics *metrics) {
 
 	log.Printf("Connecting to %s", url)
 	//Generate TLS credentials from directory
 	creds, err := credentials.NewClientTLSFromFile("/Users/joseap/.polar/networks/1/volumes/lnd/alice/tls.cert", "")
 
-	if err != nil{
+	if err != nil {
 		log.Fatalf("Failed to load credentials: %v", err)
 	}
 
@@ -42,7 +102,7 @@ func connectToServer(url string) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	md := metadata.New(map[string]string{"macaroon": "0201036c6e6402f801030a108be5b2928f746a822b04a9b2848eb0321201301a160a0761646472657373120472656164120577726974651a130a04696e666f120472656164120577726974651a170a08696e766f69636573120472656164120577726974651a210a086d616361726f6f6e120867656e6572617465120472656164120577726974651a160a076d657373616765120472656164120577726974651a170a086f6666636861696e120472656164120577726974651a160a076f6e636861696e120472656164120577726974651a140a057065657273120472656164120577726974651a180a067369676e6572120867656e6572617465120472656164000006208e8b02d4bc0efd4f15a52946c5ef23f2954f8a07ed800733554a11a190cb71b4"})
+	md := metadata.New(map[string]string{"macaroon": "0201036c6e6402ac01030a108ae5b2928f746a822b04a9b2848eb0321201301a0f0a07616464726573731204726561641a0c0a04696e666f1204726561641a100a08696e766f696365731204726561641a100a086d616361726f6f6e1204726561641a0f0a076d6573736167651204726561641a100a086f6666636861696e1204726561641a0f0a076f6e636861696e1204726561641a0d0a0570656572731204726561641a0e0a067369676e657212047265616400000620dfb922212dc2831973c90712913f0bfea68916a640c4d8475359ea593e6789ea"})
 
 	context := metadata.NewOutgoingContext(ctx, md)
 
@@ -58,16 +118,10 @@ func connectToServer(url string) {
 	//Iterate over response channels
 	for _, channel := range response.Channels {
 		//Print channel id
-		log.Printf("Channel ID: %d", channel.ChanId)
 
-		//Get the balance of the channel
-		localBalance := channel.LocalBalance
-		remoteBalance := channel.RemoteBalance
+		//Record the channel balance in a prometheus gauge
+		recordChannelBalance(metrics, channel)
 
-		//Print the balance of the channel
-		log.Printf("Local Balance: %d", localBalance)
-		log.Printf("Remote Balance: %d", remoteBalance)
 	}
-
 
 }
