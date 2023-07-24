148f39ec80beed3ff81749aeaa102ebb8c94dfbd
José A.P <53834183+Jossec101@users.noreply.github.com>
2023-07-24 15:31:23 +0200

    Update flags.go

--- flags.go
+++ flags.go
@@ -97,7 +97,7 @@ func init() {
 	viper.BindPFlag("nodeguardHost", rootCmd.Flags().Lookup("nodeguardHost"))
 
 	//Swap Publication Offset in minutes
-	rootCmd.Flags().String("swapPublicationOffset", "30m", "Swap publication deadline offset (Maximum time for the swap provider to publish the swap))))")
+	rootCmd.Flags().String("swapPublicationOffset", "30m", "Swap publication deadline offset (Maximum time for the swap provider to publish the swap)")
 	viper.BindPFlag("swapPublicationOffset", rootCmd.Flags().Lookup("swapPublicationOffset"))
 
 	//Now we set the global vars
