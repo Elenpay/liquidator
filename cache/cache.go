16dbf388a2187d2adba59045695873ce2114b837
Rodrigo <39995243+RodriFS@users.noreply.github.com>
2023-08-25 09:29:40 +0200

    Added log to cache when no rule is found

--- cache/cache.go
+++ cache/cache.go
@@ -83,6 +83,7 @@ func (c *BigCache) GetLiquidityRules(nodePubkey string) (map[uint64][]nodeguard.
 
 	entry, err := c.cache.Get(nodePubkey)
 	if err != nil {
+		log.Debugf("Unable to get liquidity rule for %s: %s", nodePubkey, err)
 		return nil, nil
 	}
 
