013c7db16aaeea0bf77984ef7d0ac864113dabbb
Marcos <marferp2@inf.upv.es>
2023-11-17 11:08:18 +0100

    fix(liquidator_test.go): change walletId and address variables from pointers to values to improve code readability and semantics

--- liquidator_test.go
+++ liquidator_test.go
@@ -110,10 +110,8 @@ func Test_manageChannelLiquidity(t *testing.T) {
 	mockProviderInvalidLoopError := createMockProviderInvalidSwapLoopError(mockCtrl)
 
 	// Wallet and address for reverse swaps
-	var walletId *int32 = new(int32)
-	*walletId = 1
-	var address *string = new(string)
-	*address = "bcrt1q6zszlnxhlq0lsmfc42nkwgqedy9kvmvmxhkvme"
+	var walletId int32 = 1
+	var address string = "bcrt1q6zszlnxhlq0lsmfc42nkwgqedy9kvmvmxhkvme"
 
 	//Active channel
 	channelActive := &lnrpc.Channel{
@@ -148,7 +146,7 @@ func Test_manageChannelLiquidity(t *testing.T) {
 			args: ManageChannelLiquidityInfo{
 				channel:             channelActive,
 				channelBalanceRatio: 0.1,
-				channelRules:        &[]nodeguard.LiquidityRule{{ChannelId: 123, NodePubkey: "", IsReverseSwapWalletRule: true, ReverseSwapWalletId: walletId, MinimumLocalBalance: 20, MinimumRemoteBalance: 80, RebalanceTarget: 60}},
+				channelRules:        &[]nodeguard.LiquidityRule{{ChannelId: 123, NodePubkey: "", IsReverseSwapWalletRule: true, ReverseSwapWalletId: &walletId, MinimumLocalBalance: 20, MinimumRemoteBalance: 80, RebalanceTarget: 60}},
 				nodeguardClient:     mockNodeGuardClient,
 				loopProvider:        mockProvider,
 				loopdMacaroon:       "0201036c6e6402f801030a10dc64226b045d25f090b114baebcbf04c1201301a160a0761646472657373120472656164120577726974651a130a04696e666f120472656164120577726974651a170a08696e766f69636573120472656164120577726974651a210a086d616361726f6f6e120867656e6572617465120472656164120577726974651a160a076d657373616765120472656164120577726974651a170a086f6666636861696e120472656164120577726974651a160a076f6e636861696e120472656164120577726974651a140a057065657273120472656164120577726974651a180a067369676e6572120867656e657261746512047265616400000620a21b8cc8c071aa5104b706b751aede972f642537c05da31450fb4b02c6da776e",
@@ -162,7 +160,7 @@ func Test_manageChannelLiquidity(t *testing.T) {
 			args: ManageChannelLiquidityInfo{
 				channel:             channelActive,
 				channelBalanceRatio: 0.1,
-				channelRules:        &[]nodeguard.LiquidityRule{{ChannelId: 123, NodePubkey: "", IsReverseSwapWalletRule: false, ReverseSwapAddress: address, MinimumLocalBalance: 20, MinimumRemoteBalance: 80, RebalanceTarget: 60}},
+				channelRules:        &[]nodeguard.LiquidityRule{{ChannelId: 123, NodePubkey: "", IsReverseSwapWalletRule: false, ReverseSwapAddress: &address, MinimumLocalBalance: 20, MinimumRemoteBalance: 80, RebalanceTarget: 60}},
 				nodeguardClient:     mockNodeGuardClient,
 				loopProvider:        mockProvider,
 				loopdMacaroon:       "0201036c6e6402f801030a10dc64226b045d25f090b114baebcbf04c1201301a160a0761646472657373120472656164120577726974651a130a04696e666f120472656164120577726974651a170a08696e766f69636573120472656164120577726974651a210a086d616361726f6f6e120867656e6572617465120472656164120577726974651a160a076d657373616765120472656164120577726974651a170a086f6666636861696e120472656164120577726974651a160a076f6e636861696e120472656164120577726974651a140a057065657273120472656164120577726974651a180a067369676e6572120867656e657261746512047265616400000620a21b8cc8c071aa5104b706b751aede972f642537c05da31450fb4b02c6da776e",
@@ -176,7 +174,7 @@ func Test_manageChannelLiquidity(t *testing.T) {
 			args: ManageChannelLiquidityInfo{
 				channel:             channelActive,
 				channelBalanceRatio: 0.1,
-				channelRules:        &[]nodeguard.LiquidityRule{{ChannelId: 123, NodePubkey: "", IsReverseSwapWalletRule: true, ReverseSwapWalletId: walletId, MinimumLocalBalance: 20, MinimumRemoteBalance: 80, RebalanceTarget: 40}},
+				channelRules:        &[]nodeguard.LiquidityRule{{ChannelId: 123, NodePubkey: "", IsReverseSwapWalletRule: true, ReverseSwapWalletId: &walletId, MinimumLocalBalance: 20, MinimumRemoteBalance: 80, RebalanceTarget: 40}},
 				nodeguardClient:     mockNodeGuardClient,
 				loopProvider:        mockProvider,
 				loopdMacaroon:       "0201036c6e6402f801030a10dc64226b045d25f090b114baebcbf04c1201301a160a0761646472657373120472656164120577726974651a130a04696e666f120472656164120577726974651a170a08696e766f69636573120472656164120577726974651a210a086d616361726f6f6e120867656e6572617465120472656164120577726974651a160a076d657373616765120472656164120577726974651a170a086f6666636861696e120472656164120577726974651a160a076f6e636861696e120472656164120577726974651a140a057065657273120472656164120577726974651a180a067369676e6572120867656e657261746512047265616400000620a21b8cc8c071aa5104b706b751aede972f642537c05da31450fb4b02c6da776e",
@@ -190,7 +188,7 @@ func Test_manageChannelLiquidity(t *testing.T) {
 			args: ManageChannelLiquidityInfo{
 				channel:             channelActive,
 				channelBalanceRatio: 0.1,
-				channelRules:        &[]nodeguard.LiquidityRule{{ChannelId: 123, NodePubkey: "", IsReverseSwapWalletRule: true, ReverseSwapWalletId: walletId, MinimumLocalBalance: 20, MinimumRemoteBalance: 80, RebalanceTarget: 40}},
+				channelRules:        &[]nodeguard.LiquidityRule{{ChannelId: 123, NodePubkey: "", IsReverseSwapWalletRule: true, ReverseSwapWalletId: &walletId, MinimumLocalBalance: 20, MinimumRemoteBalance: 80, RebalanceTarget: 40}},
 				nodeguardClient:     mockNodeGuardClient,
 				loopProvider:        mockProvider,
 				loopdMacaroon:       "0201036c6e6402f801030a10dc64226b045d25f090b114baebcbf04c1201301a160a0761646472657373120472656164120577726974651a130a04696e666f120472656164120577726974651a170a08696e766f69636573120472656164120577726974651a210a086d616361726f6f6e120867656e6572617465120472656164120577726974651a160a076d657373616765120472656164120577726974651a170a086f6666636861696e120472656164120577726974651a160a076f6e636861696e120472656164120577726974651a140a057065657273120472656164120577726974651a180a067369676e6572120867656e657261746512047265616400000620a21b8cc8c071aa5104b706b751aede972f642537c05da31450fb4b02c6da776e",
@@ -227,7 +225,7 @@ func Test_manageChannelLiquidity(t *testing.T) {
 			args: ManageChannelLiquidityInfo{
 				channel:             channelActive,
 				channelBalanceRatio: 0.1,
-				channelRules:        &[]nodeguard.LiquidityRule{{ChannelId: 123, NodePubkey: "", IsReverseSwapWalletRule: true, ReverseSwapWalletId: walletId, MinimumLocalBalance: 20, MinimumRemoteBalance: 80, RebalanceTarget: 60}},
+				channelRules:        &[]nodeguard.LiquidityRule{{ChannelId: 123, NodePubkey: "", IsReverseSwapWalletRule: true, ReverseSwapWalletId: &walletId, MinimumLocalBalance: 20, MinimumRemoteBalance: 80, RebalanceTarget: 60}},
 				nodeguardClient:     mockNodeGuardClient,
 				loopProvider:        mockProviderInvalid,
 				loopdMacaroon:       "0201036c6e6402f801030a10dc64226b045d25f090b114baebcbf04c1201301a160a0761646472657373120472656164120577726974651a130a04696e666f120472656164120577726974651a170a08696e766f69636573120472656164120577726974651a210a086d616361726f6f6e120867656e6572617465120472656164120577726974651a160a076d657373616765120472656164120577726974651a170a086f6666636861696e120472656164120577726974651a160a076f6e636861696e120472656164120577726974651a140a057065657273120472656164120577726974651a180a067369676e6572120867656e657261746512047265616400000620a21b8cc8c071aa5104b706b751aede972f642537c05da31450fb4b02c6da776e",
@@ -255,7 +253,7 @@ func Test_manageChannelLiquidity(t *testing.T) {
 			args: ManageChannelLiquidityInfo{
 				channel:             channelActive,
 				channelBalanceRatio: 0.1,
-				channelRules:        &[]nodeguard.LiquidityRule{{ChannelId: 123, NodePubkey: "", IsReverseSwapWalletRule: true, ReverseSwapWalletId: walletId, MinimumLocalBalance: 20, MinimumRemoteBalance: 80, RebalanceTarget: 60}},
+				channelRules:        &[]nodeguard.LiquidityRule{{ChannelId: 123, NodePubkey: "", IsReverseSwapWalletRule: true, ReverseSwapWalletId: &walletId, MinimumLocalBalance: 20, MinimumRemoteBalance: 80, RebalanceTarget: 60}},
 				nodeguardClient:     mockNodeGuardClient,
 				loopProvider:        mockProviderInvalidLoopError,
 				loopdMacaroon:       "0201036c6e6402f801030a10dc64226b045d25f090b114baebcbf04c1201301a160a0761646472657373120472656164120577726974651a130a04696e666f120472656164120577726974651a170a08696e766f69636573120472656164120577726974651a210a086d616361726f6f6e120867656e6572617465120472656164120577726974651a160a076d657373616765120472656164120577726974651a170a086f6666636861696e120472656164120577726974651a160a076f6e636861696e120472656164120577726974651a140a057065657273120472656164120577726974651a180a067369676e6572120867656e657261746512047265616400000620a21b8cc8c071aa5104b706b751aede972f642537c05da31450fb4b02c6da776e",
